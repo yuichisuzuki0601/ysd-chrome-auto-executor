61c24fafc7c3bb537f846d99931886659053f3c6
yuichi <ys.development0601@gmail.com>
2022-09-21 05:06:33 +0900

    ページごとにボタン押下時の処理を変える

--- content.js
+++ content.js
@@ -1,12 +1,20 @@
 chrome.runtime.onMessage.addListener(async (_args, _sender, _sendResponse) => {
-  const { systems, doms } = ysd;
   console.log("script start.");
-  // ↓↓↓ any process you wants.
-
-  doms.input("#input1", "hoge");
-  await systems.sleep(3);
-  doms.click("#button1");
-
-  // ↑↑↑ any process you wants.
+  await PATH_FUNCTIONS[ysd.systems.simplePath()]?.(ysd);
   console.log("script finish.");
 });
+
+// ↓↓↓ any process you wants.
+const PATH_FUNCTIONS = {
+  "page-1.html": async ({ systems, doms }) => {
+    doms.input("#input-data", "hoge");
+    await systems.sleep(2);
+    doms.click("#button-alert");
+    doms.click("#link-next");
+  },
+  "page-2.html": async ({ systems, doms }) => {
+    await systems.sleep(1);
+    doms.click("#link-back");
+  },
+};
+// ↑↑↑ any process you wants.
--- libs/systems.js
+++ libs/systems.js
@@ -1,12 +1,17 @@
 window.ysd = window.ysd || {};
 
 window.ysd.systems = {
-  sleep: (sec) =>
-    new Promise((resolve) => {
+  sleep: (sec) => {
+    return new Promise((resolve) => {
       const intervalId = setInterval(() => console.log("sleeping..."), 1000);
       setTimeout(() => {
         clearInterval(intervalId);
         resolve();
       }, sec * 1000);
-    }),
+    });
+  },
+  simplePath: () => {
+    const tmp = location.pathname.split("/");
+    return tmp[tmp.length - 1];
+  },
 };
